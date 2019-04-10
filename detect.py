633bb7654bfd9d8073229d011c6f122c4dd4191d
Lucas Vandroux <lucas.vandroux@gmail.com>
2019-04-10 11:24:35 +0200

    Beautify the output of the model in the terminal

--- detect.py
+++ detect.py
@@ -14,23 +14,29 @@ img = Image.open(image_path)
 # --- LOAD ROCKET ---
 # Select the Rocket you want to test
 rocket = "lucas/ssd"
-# rocket = "igor/retinanet"
+rocket = "igor/retinanet"
 # rocket = "lucas/yolov3"
 
 model = Rocket.land(rocket).eval()
 
 # --- DETECTION ---
+print('Using the rocket to do object detection on \'' + image_path + '\'...')
 with torch.no_grad():
     img_tensor = model.preprocess(img)
     out = model(img_tensor)
 
+print('Object Detection successful! ')
+
 # --- OUTPUT ---
 # Print the output as a JSON
 bboxes_out = model.postprocess(out, img)
-print(bboxes_out)
+print(len(bboxes_out), 'different objects were detected:')
+print(*bboxes_out, sep='\n')
 
 # Display the output over the image
 img_out = model.postprocess(out, img, visualize=True)
-img_out.save("out.jpg")
+img_out_path = 'out.jpg'
+img_out.save(img_out_path)
+print('You can see the detections on the image: \'' + img_out_path +'\'.')
 
 
